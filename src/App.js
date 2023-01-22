fb43ec3fc8eeced480fc953916f8afa39e4be4b3
Cristian Henriquez Urbina <cristian.henriquez.urbina6@gmail.com>
2023-01-22 13:48:22 -0300

    Desafio estados componentes y eventos react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Boton from './Components/Boton';
+
 
 function App() {
+  const [user,setNombre]= useState('');
+  const [pass,setPass] = useState('');
+  const [mensaje,setMensaje] =useState('');
+  const [desplegar,setDesplegar]= useState(false);
+
+    const handleSubmit =(e)=>{
+    e.preventDefault();
+    setDesplegar(true);
+    if (user ==="ADL" && pass ==="252525"){
+        setMensaje('los datos son correctos')
+    }else{
+        setMensaje('los datos son incorrectos  ')
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container w-50">
+
+       <div>
+         <h1 className = 'text-center'>Autentificación Componentes</h1>
+       </div>
+       {desplegar && <div class="alert alert-primary" role="alert">
+                          {mensaje}
+                      </div>}
+       <form onSubmit={handleSubmit}>
+          <div>
+              <label htmlFor='name'>Nombre</label>
+              <input type='text'
+                      id='name' 
+                      defaultValue = {user} 
+                      onChange={(e)=> {
+                            setNombre(e.target.value);
+                      }}
+                      className='form-control' >
+              </input>
+          </div>
+          
+          <div>
+              <label htmlFor='password'>password</label>
+              <input type = 'password' 
+                     id='password' 
+                     defaultValue = {pass}  
+                     onChange={(e)=>{
+                           setPass(e.target.value);
+                     }}
+                     className='form-control'>
+              </input>
+          </div>
+          
+          <div className='mt-3 text-center'>
+              {user.length > 0 && pass.length > 0 && <Boton texto = "Iniciar Sessión"/>}
+                  
+          </div>
+      </form>
     </div>
   );
 }
